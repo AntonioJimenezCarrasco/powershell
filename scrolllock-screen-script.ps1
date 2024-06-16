724113a4d7f41b4d24de1faadefcca4b3579bbca
Antonio Jiménez Carrasco <43549066+AntonioJimenezCarrasco@users.noreply.github.com>
2024-06-16 18:49:40 +0200

    Update scrolllock-screen-script.ps1

--- scrolllock-screen-script.ps1
+++ scrolllock-screen-script.ps1
@@ -1,7 +1,10 @@
-Clear-Host
-$WShell = New-Object -com "Wscript.Shell"
-while($true) {
-$WShell.sendkeys("{SCROLLLOCK}")
-Start-Sleep -Milliseconds 50
-$WShell.sendkeys("{SCROLLLOCK}")
-Start-Sleep -Seconds 120
+Clear-Host #not strictly necessary
+$WShell = New-Object -ComObject "Wscript.Shell"
+while ($true) {
+    $WShell.sendkeys("{SCROLLLOCK}")
+    Start-Sleep -Milliseconds 50
+    $WShell.sendkeys("{SCROLLLOCK}")
+    Start-Sleep -Seconds 120
+}
+#use this script with caution
+#while not harmful per se, keeping a system endlessly alive isn't the greatest idea
